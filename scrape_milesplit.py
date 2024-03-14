b8405f4380cc9f067077ea8ad1d183ce73ef6204
mitchellggreenhalgh <63256822+mitchellggreenhalgh@users.noreply.github.com>
2024-03-14 15:36:48 -0500

    migrated everything from webpy repo

---
+++ scrape_milesplit.py
@@ -0,0 +1,379 @@
+'''A module for a MileSplitScraper class that uses selenium to scrape 800m and 400m data from milesplit.com'''
+
+import pandas as pd
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+from dotenv import dotenv_values
+from io import StringIO
+
+
+FIRST_URL = 'https://www.milesplit.com/rankings/events/high-school-girls/indoor-track-and-field/800m?year=2024&accuracy=fat&grade=all&ageGroup=&league=0&meet=0&team=0&venue=0&conversion=n&page=1'
+
+config = dotenv_values('.env')
+
+
+class MileSplitScraper():
+    '''A class to download 800m and 400m data from the National MileSplit database.'''
+
+    def __init__(self, url: str=FIRST_URL):
+        '''Initialize the MileSplitScraper class
+        
+        Parameters:
+          -  url (str): a URL to the milesplit rankings portion of the website to log in on and start scraping
+        '''
+
+        self.url = url
+        self.website = 'https://www.milesplit.com/'
+        self.USERNAME = config['USERNAME']
+        self.PASSWORD = config['PASSWORD']
+
+        self.OPTIONS = Options()
+        self.OPTIONS.add_argument('--ignore-certificate-errors')
+        self.OPTIONS.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.OPTIONS.add_argument("--disable-blink-features=AutomationControlled")
+
+
+    def log_in(self, url: str | None=None, options: Options | None=None) -> webdriver.Chrome:
+        '''Uses a Milesplit URL to log in
+
+        This could be optimized a lot I think. There are lots of 'time.sleep()' calls to prevent JS detectors from kicking the the program out, and some of them could be removed or minimized. The login isn't always successful, sometimes it just doesn't enter the keys, and I'm not sure how to automate checking if the login was successful and the program is running.
+        
+        Parameters:
+          -  url (str): the URL to access the login
+          -  options (Options): Selenium Chrome Driver options
+
+        Returns:
+          -  driver (webdriver.Chrome): the driver being used to navigate MileSplit
+        '''
+
+        if url is None:
+            url = self.url
+        
+        if options is None:
+            options = self.OPTIONS
+
+        # Start Browser and Driver
+        webdriver_service = Service(executable_path=r"C:\Users\mitch\Documents\Programs\chromedriver\chrome-win32-121\chromedriver-win32\chromedriver.exe")
+        driver = webdriver.Chrome(service=webdriver_service, options=options)
+        driver.implicitly_wait(30)
+
+        # Log in sequence
+        driver.get(url)
+
+        # Let ads load
+        time.sleep(5)
+
+        # Click account button
+        account_button = driver.find_element(By.XPATH, '//*[@id="account"]/div[1]')
+        account_button.click()
+        time.sleep(2)
+
+        # Click login link
+        login_link = driver.find_element(By.XPATH, '//*[@id="account"]/div[2]/section/ul/li[1]/a')
+        login_link.click()
+        time.sleep(5)
+
+        # Enter Username and Password
+        email = driver.find_element(By.ID, 'email')
+        email.send_keys(self.USERNAME)
+        time.sleep(1)
+
+        password = driver.find_element(By.ID, 'password')
+        password.send_keys(self.PASSWORD)
+        time.sleep(1)
+
+        # Submit form
+        submit = driver.find_element(By.ID, 'frmSubmit')
+        submit.click()
+
+        # Let login process
+        time.sleep(3)
+
+        return driver
+
+
+    def create_url(self,
+                   level: str, 
+                   sex: str, 
+                   season: str, 
+                   event: str, 
+                   year: str, 
+                   page: int) -> str:
+        '''Use the different variables in the milesplit url to make the url for the next download.
+        
+        Parameters:
+          -  level (str): 'middle' for middle school or 'high' for high school
+          -  sex (str): 'girls' or 'boys'
+          -  season (str): 'indoor' or 'outdoor'
+          -  event (str): typically '800m', but also takes '400m'
+          -  year (str): 'yyyy', the year of the season (2000-2024)
+          -  page (int): options are 1 - 20
+
+        Returns:
+          -  url (str): the full URL for the Milesplit page to download
+        '''
+        
+        # TODO: #3 Add state functionality, does the rest of the URL stay the same when you add the state prefix?
+        url = f'https://www.milesplit.com/rankings/events/{level}-school-{sex}/{season}-track-and-field/{event}?year={year}&accuracy=fat&grade=all&ageGroup=&league=0&meet=0&team=0&venue=0&conversion=n&page={page}'
+
+        return url
+
+
+    def clean_400m_times(self, row: str | float) -> float:
+        '''Take each row of a pd.Series and modify it according to its format
+        
+        Parameters:
+          -  row (str | float): Pages with everything below 1 minute comes in as a float, a page witheverything above comes as a string. If a page of results has both under 1 minute and above 1 minute on the same page, they all come as a string.
+        '''
+
+        if str(row)[0] in ['4', '5']:
+            return row
+        else:
+            return 60 * int(str(row)[0]) + float(str(row)[2:7])
+
+
+    def download_and_clean(self, data: pd.DataFrame, event: str, season: str, year: str) -> pd.DataFrame:
+        '''Takes the output from the page content and cleans it up
+        
+        Parameters:
+          -  data (pd.DataFrame): The pd.DataFrame returned from the page source
+          -  event (str): typically '800m', but also takes '400m'
+          -  season (str): 'indoor' or 'outdoor'
+          -  year (str): 'yyyy', the year of the season (2000-2024)
+            
+        Returns
+          -  df (pd.DataFrame): a pd.DataFrame that's been cleaned and ready for concatenation with other DataFrames
+        '''
+
+        df = data
+        df.columns = df.columns.str.lower()
+
+        # combine id columns
+        df['athlete_team_grade'] = df['athlete/team'] + ' ' + df['grade'].astype('str')
+        df['athlete_team_grade'] = df['athlete_team_grade'].str.replace('.0', '')
+
+        # Process time column
+        if event == '800m':
+            df[f'time_{event[:-1]}'] = 60 * df['time'].str[0].astype('int') + df['time'].str[2:7].astype('float')
+        elif event == '400m':
+            df[f'time_{event[:-1]}'] = df['time'].apply(self.clean_400m_times)
+
+        # Drop extra columns
+        df = df.drop(columns = ['rank', 'athlete/team', 'grade', 'meet  date  place', 'time'])
+
+        # Add season info
+        df['season'] = f'{season}_{year}'
+
+        return df[['athlete_team_grade', f'time_{event[:-1]}', 'season']]
+        
+
+    def download_single_event(self,
+                              driver: webdriver.Chrome, 
+                              level: str, 
+                              sex: str, 
+                              season: str, 
+                              event: str, 
+                              year: str) -> pd.DataFrame:
+        '''Download all 20 pages of an event of milesplit data for a single sex
+        
+        Parameters:
+          -  driver (webdriver.Chrome): the webdriver used to log in
+          -  level (str): 'middle' for middle school or 'high' for high school
+          -  sex (str): 'girls' or 'boys'
+          -  season (str): 'indoor' or 'outdoor'
+          -  event (str): typically '800m' and '400m', but also takes any other valid track event
+          -  year (str): 'yyyy', the year of the season (2000-2024)
+
+        Returns:
+          -  dfs (pd.DataFrame): a pd.DataFrame of all 20 pages of event data that belongs to a single sex
+        '''
+
+        dfs = None
+
+        # TODO: For full production, turn the range from 1 -> 21
+        for page in range(1, 21, 1):
+            # Create URL
+            url = self.create_url(level, sex, season, event, year, page)
+            driver.get(url)
+
+            # Let the page and ads load
+            time.sleep(5)
+
+            # Grab and download page content
+            content = driver.page_source
+            df = pd.read_html(StringIO(content))[0].pipe(self.download_and_clean, event=event, season=season, year=year)
+
+            if dfs is None:
+                dfs = df
+                continue
+
+            dfs = pd.concat([dfs, df])
+
+        return dfs
+
+
+    def download_both_events(self,
+                             driver: webdriver.Chrome, 
+                             level: str, 
+                             sex: str, 
+                             season: str, 
+                             year: str) -> pd.DataFrame:
+        '''Download 800m and 400m data from Milesplit and join the tables together
+        
+        Parameters:
+          -  driver (webdriver.Chrome): the webdriver used to log in
+          -  level (str): 'middle' for middle school or 'high' for high school
+          -  sex (str): 'girls' or 'boys'
+          -  season (str): 'indoor' or 'outdoor'
+          -  year (str): 'yyyy', the year of the season (2000-2024)
+
+        Returns:
+          -  df (pd.DataFrame): a pd.DataFrame of both 800m and 400m data that belongs to a single sex for a single season
+        '''
+
+        # TODO: #5 Add 1600m functionality
+
+        df_800 = self.download_single_event(driver=driver,
+                                            level=level,
+                                            sex=sex,
+                                            season=season,
+                                            event='800m',
+                                            year=year)
+        
+        df_400 = self.download_single_event(driver=driver,
+                                            level=level,
+                                            sex=sex,
+                                            season=season,
+                                            event='400m',
+                                            year=year)
+        
+        df = df_800.merge(df_400, how = 'left', left_on=['athlete_team_grade', 'season'], right_on=['athlete_team_grade', 'season']) \
+            .dropna()
+        
+        return df[['athlete_team_grade', 'time_800', 'time_400', 'season']]
+
+
+    def combine_sexes(self,
+                      driver: webdriver.Chrome, 
+                      level: str, 
+                      season: str, 
+                      year: str) -> pd.DataFrame:
+        '''Download 800m and 400m data from Milesplit for both sexes and combines them together
+        
+        Parameters:
+          -  driver (webdriver.Chrome): the webdriver used to log in
+          -  level (str): 'middle' for middle school or 'high' for high school
+          -  season (str): 'indoor' or 'outdoor'
+          -  year (str): 'yyyy', the year of the season (2000-2024)
+
+        Returns:
+          -  df (pd.DataFrame): a pd.DataFrame of both 800m and 400m data that for both sexes for a single season
+        '''
+
+        df_f = self.download_both_events(driver, level, 'girls', season, year)
+        df_m = self.download_both_events(driver, level, 'boys', season, year)
+
+        df = pd.concat([df_f, df_m])
+
+        return df
+
+
+    def download_seasons(self,
+                         driver: webdriver.Chrome, 
+                         level: str, 
+                         year: str) -> pd.DataFrame:
+        '''Download 800m and 400m data from Milesplit for both sexes and combines them together
+        
+        Parameters:
+          -  driver (webdriver.Chrome): the webdriver used to log in
+          -  level (str): 'middle' for middle school or 'high' for high school
+          -  year (str): 'yyyy', the year of the season (2000-2024)
+
+        Returns:
+          -  df (pd.DataFrame): a pd.DataFrame of both 800m and 400m data that for both sexes for a indoor and outdoor of a certain year for a certain level of competition
+        '''
+
+        df_indoor = self.combine_sexes(driver=driver, level=level, season='indoor', year=year)
+        df_outdoor = self.combine_sexes(driver=driver, level=level, season='outdoor', year=year)
+
+        df = pd.concat([df_indoor, df_outdoor])
+
+        return df
+                    
+
+    def download_levels(self,
+                        driver: webdriver.Chrome, 
+                        year: str) -> pd.DataFrame:
+        '''Download 800m and 400m data from Milesplit for both sexes in middle school and high school and combines them together
+        
+        Parameters:
+          -  driver (webdriver.Chrome): the webdriver used to log in
+          -  year (str): 'yyyy', the year of the season (2000-2024)
+
+        Returns:
+          -  df (pd.DataFrame): a pd.DataFrame of both 800m and 400m data that for both sexes for a indoor and outdoor of a certain year for both middle school and high school
+        '''
+
+        df_hs = self.download_seasons(driver=driver, year=year, level='high')
+        df_ms = self.download_seasons(driver=driver, year=year, level='middle')
+        
+        df = pd.concat([df_hs, df_ms])
+
+        return df
+
+
+    def download_years(self, driver: webdriver.Chrome, start: int, end: int) -> pd.DataFrame:
+        '''Download 800m and 400m data from Milesplit for both sexes in middle school and high school and combines them together for multiple years of data
+        
+        Parameters:
+          -  driver (webdriver.Chrome): the webdriver used to log in
+          -  start (int): [yyyy], the first year to be downloaded
+          -  end (int): [yyyy], the last year to be downloaded
+
+        Returns:
+          -  dfs (pd.DataFrame): a pd.DataFrame of both 800m and 400m data that for both sexes for a indoor and outdoor for all specified years for both middle school and high school
+        '''
+
+        dfs = None
+
+        for year in range(start, end + 1, 1):
+            df = self.download_levels(driver=driver, year=str(year))
+
+            if dfs is None:
+                dfs = df
+                continue
+
+            dfs = pd.concat([dfs, df])
+        
+        dfs.drop_duplicates()
+        
+        return dfs
+
+
+    def download_and_export(self, start: int, end: int) -> pd.DataFrame:
+        '''Run the whole scraping program for all levels, all sexes, all events, over the specified range of years
+
+        Parameters:
+          -  start (int): [yyyy], the first year to be downloaded
+          -  end (int): [yyyy], the last year to be downloaded
+
+        Returns:
+          -  df (pd.DataFrame): A pd.DataFrame of all the MS and HS results from indoor and outdoor of a specified range of years
+        '''
+
+        driver = self.log_in()
+        df = self.download_years(driver, start, end)
+        driver.close()
+        df.to_csv(f'milesplit_indoor_{start}-outdoor_{end}.csv', index=False)
+
+        return df
+
+
+if __name__ == '__main__':
+    ms = MileSplitScraper()
+    ms.download_and_export(2020, 2024)
+
+# TODO: #4 Make dictionary of season and available pages
---
+++ scrape_tfrrs.py
@@ -0,0 +1,159 @@
+'''A module to create a TFRRSScraper class to scrape 800m and 400m data from TFRRS.org'''
+
+# TODO: #2 Add 1500m Functionality
+
+import pandas as pd
+from glob import glob
+
+class TFRRSScraper():
+    def __init__(self):
+        self.website = 'https://tfrrs.org/'
+
+    def __repr__(self):
+        return f'Scraper object to scrape data from [{self.website}]'
+
+    def extract_event_data(self, url_root_800: str, url_root_400: str, sex: str) -> pd.DataFrame:
+        '''Extract data from tfrrs.org.
+        
+        Parameters:
+          -  url_root_800 (str): URL to a TFRRS 800m table
+          -  url_root_400 (str): URL to a TFRRS 400m table
+          -  sex (str): 'f' for women and 'm' for men
+
+        Returns:
+          -  df (pd.DataFrame): DataFrame containing athletes who ran the 400 and 800 during the 2024 indoor season and had both times in the top 500 of Division I performances
+        '''
+
+        df_800 = pd.read_html(f'{url_root_800[:-1]}{sex}',
+                            attrs={'id': 'myTable'})[0][['Athlete', 'Team', 'Time']]
+
+        df_400 = pd.read_html(f'{url_root_400[:-1]}{sex}',
+                            attrs={'id': 'myTable'})[0][['Athlete', 'Team', 'Time']]
+
+        # Merge DataFrames
+        df = df_800.merge(df_400, how = 'left', left_on=['Athlete', 'Team'], right_on=['Athlete', 'Team']) \
+            .dropna()
+
+        # Translate 800m time to seconds, remove '@' and '#' tags
+        df['Time_x'] = 60 * df['Time_x'].str[0].astype('float') + df['Time_x'].str[2:7].astype('float')
+
+        # Translate 400m time to seconds if necessary, remove '@' and '#' tags in 400m
+        if df['Time_y'].dtype == 'O':
+            df['Time_y'] = df['Time_y'].apply(self.clean_400m_times)
+
+        # Make columns more intuitive
+        df.columns = ['athlete', 'team', 'time_800', 'time_400']
+
+        df['athlete_team'] = df['athlete'] + ' ' + df['team']
+        df = df.drop(columns = ['athlete', 'team'])
+
+        return df[['athlete_team', 'time_800', 'time_400']]
+
+
+    def clean_400m_times(self, row: str | float) -> float:
+        '''Take each row of a pd.Series and modify it according to its format
+        
+        Parameters:
+          -  row (str | float): the value of the row in the pd.Series
+        '''
+
+        if str(row)[0] in ['4', '5']:
+            return float(str(row)[:5])
+        else:
+            return 60 * int(str(row)[0]) + float(str(row)[2:7])
+
+
+    def merge_data(self, url_root_800: str, url_root_400: str) -> pd.DataFrame:
+        '''Merges TFRRS data tables from the different sex categories together.
+        
+        Parameters:
+          -  url_root_800 (str): URL to a TFRRS 800m table
+          -  url_root_400 (str): URL to a TFRRS 400m table
+            
+        Returns:
+          -  df_all (pd.DataFrame): a pd.DataFrame that merges the 400/800 data for both sexes into one table.
+        '''
+        
+        df_women = self.extract_event_data(url_root_400=url_root_400, url_root_800=url_root_800, sex='f')
+        df_men = self.extract_event_data(url_root_400=url_root_400, url_root_800=url_root_800, sex='m')
+
+        df_all = pd.concat([df_women, df_men])
+
+        return df_all
+
+
+    def download_urls(self, url_root: str, season: str) -> pd.DataFrame:
+        '''Merges TFRRS 400m and 800m data tables from the different sex categories together.
+        
+        Parameters:
+          -  url_root_800 (str): URL to a TFRRS 800m table
+          -  season (str): 'indoor' or 'outdoor'. Depending on the season, the values for the events change. In indoor: 53 = 400m, 54 = 800m. In outdoor: 11 = 400m, 12 = 800m.
+            
+            
+        Returns:
+          -  df (pd.DataFrame): a pd.DataFrame that merges the 400/800 data for both sexes into one table.
+        '''
+
+        if season == 'indoor':
+            df = self.merge_data(url_root_800 = url_root,
+                                 url_root_400 = url_root.replace('event_type=54', 'event_type=53'))
+        elif season == 'outdoor':
+            df = self.merge_data(url_root_800 = url_root,
+                                 url_root_400 = url_root.replace('event_type=12', 'event_type=11'))
+
+        return df
+
+
+    def download_seasons(self, seasons: dict, division: str) -> pd.DataFrame:
+        '''Takes a dictionary of seasons and urls for a division and combines them all into a single pd.DataFrame
+        
+        Parameters:
+          -  seasons (dict): Keys follow the pattern [season]_[year], and the values are the URL to the Top 500 800m runners table for that season-year-division combination on TFFRS.
+          -  division (str): with NCAA Division is being downloaded
+            
+        Returns:
+          -  dfs (pd.DataFrame): a pd.DataFrame of multiple seasons of women and men's 400m and 800m times.
+        '''
+        
+        dfs = None
+
+        for season in seasons:
+            df = self.download_urls(url_root=seasons[season],
+                            season=season.split('_')[0])
+            
+            df['season'] = season
+            
+            if dfs is None:
+                dfs = df
+                continue
+
+            dfs = pd.concat([dfs, df])
+
+        # Export
+        dfs.to_csv(f'tfrrs_{division}_{list(seasons.keys())[0]}-{list(seasons.keys())[-1]}.csv', index=False)
+
+        return dfs
+
+
+    def merge_tfrrs_data(self) -> pd.DataFrame:
+        '''Grabs all the TFRRS .csv files in the root directory and concatenates them together
+        
+        Returns:
+          -  dfs (pd.DataFrame): a single pd.DataFrame of all the TFRRS data in the root directory
+        '''
+        data_list = glob('tfrrs*.csv')
+
+        dfs = None
+
+        for csv in data_list:
+            df = pd.read_csv(csv)
+
+            if dfs is None:
+                dfs = df
+                continue
+
+            dfs = pd.concat([dfs, df])
+
+        dfs.drop_duplicates()
+
+        return dfs
