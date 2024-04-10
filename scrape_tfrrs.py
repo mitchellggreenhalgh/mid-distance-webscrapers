6e5c019260157d27bf69b350b98691f7e9a397c4
mitchellggreenhalgh <63256822+mitchellggreenhalgh@users.noreply.github.com>
2024-04-09 22:01:14 -0500

    clean/convert function finished, milesplit one can definitely be simplified based on this one. outcome event section essentially finished except for the final merge option in download_and_export

--- scrape_tfrrs.py
+++ scrape_tfrrs.py
@@ -4,19 +4,18 @@ import re
 from datetime import datetime
 from glob import glob
 from typing import Dict, List
+from os import makedirs
 
 import pandas as pd
 
 __valid_events__ = [
   '100', 
+  '110H', 
   '200', 
   '400',
-  '110H', 
   '400H', 
   '800', 
   '1500', 
-  '1600', 
-  'Mile', 
   '3000', 
   '3000SC', 
   '5000', 
@@ -28,18 +27,21 @@ class TFRRSScraper():
     def __init__(self, 
                  seasons_dict: Dict[str, str],
                  *,
+                 division: str,
                  outcome_event: str, 
                  predictor_events: str | List[str] | None = None, 
-                 sex: str | None = None):
+                 sex: str | None = None,
+                 merge: bool = False):
         '''Initializes a TFRRSScraper object.
         
         Parameters:
-          -  seasons_dict (`Dict[str, str]`): A dictionary of links to the 800m TFRRS table of a given season
+          -  seasons_dict (`Dict[str, str]`): A dictionary of links to a TFRRS table of a given season. The dictionary should be in chronological order.
+          -  division (`str`): the level of collegiate competition is the dictionary for. Options: 'DI', 'DII', 'DIII', 'NAIA', 'NJCAA'
           -  predictor_event (`int`): The main event of interest. Options listed below.
           -  outcome_events (`int` | `List[str]` | `None`): Dictates which other event to download. Options: any single event or combination of the events listed below exclusive of predictor_event.
           -  sex (`str` | `None`): 'f', 'm', or None. If 'f' or 'm', download only that sex's data
 
-        Event options: '100', '200', '400', '110H', '400H', '800', '1500', '1600', 'Mile', '3000', '3200', '2Mile', '3000SC', '5000', '10000'
+        Event options: '100', '110H', '200', '400', '400H', '800', '1500', '3000', '3000SC', '5000', '10000'
 
         Regarding indoor events, '100' retrieves 60m data, '110H' retrieves 60mH data, and '1500' retrieves mile data, though the column names still express '100', '100H', and '1500'. '3000' is only offered indoor with no outdoor equivalent.
     
@@ -48,15 +50,15 @@ class TFRRSScraper():
         self.website = 'https://tfrrs.org/'
 
         self.seasons_dict = seasons_dict
+        self.division = division.lower()
         self.outcome_event = outcome_event.upper()
         self.sex = sex
+        self.merge = merge
 
         if predictor_events is None:
             self.predictor_events = None
         else:
             self.predictor_events = [predictor_events.upper()] if isinstance(predictor_events, str) else list(map(str.upper, predictor_events))
-
-        
 
         self.event_dict = {
             'indoor_60': 'event_type=46',
@@ -80,6 +82,9 @@ class TFRRSScraper():
             'outdoor_10000': 'event_type=22'   
         }
 
+        #TODO: #15 change data2 to data for full production
+        makedirs('data2', exist_ok=True)
+
         if isinstance(self.outcome_event, list):
             raise ValueError('The outcome event must be a single event of type string')
         
@@ -90,10 +95,21 @@ class TFRRSScraper():
           for event in self.predictor_events:
               if event not in __valid_events__:
                   raise ValueError(f'{event} is not a valid event. Please enter a valid track event: {__valid_events__}')
-            
+        
+        if self.division not in ['di', 'dii', 'diii', 'naia', 'njcaa']:
+            raise ValueError('Please enter an appropriate collegiate division')
+        
+        if self.sex is not None and self.sex not in ['f', 'm']:
+            raise ValueError('''Please enter an appropriate sex: 'f' or 'm' ''')
+
 
     def __repr__(self):
         return f'Scraper object for [{self.website}]'
+    
+
+    def __call__(self):
+        self.download_and_export_data()
+        return NotImplementedError
 
 
     def produce_outcome_url_by_season_key(self,
@@ -126,14 +142,14 @@ class TFRRSScraper():
         key = f'{season}_{outcome_event}'  
 
         if outcome_event in ['400H', '3000SC', '10000'] and season == 'indoor':
-            return []
+            return
         
         if outcome_event in ['3000'] and season == 'outdoor':
-            return []
+            return
 
-        url = [f'''{re.sub(pattern=r'event_type=[0-9]{2}', 
+        url = f'''{re.sub(pattern=r'event_type=[0-9]{2}', 
                           repl=self.event_dict[key], 
-                          string=base_url)[:-1]}{sex}''']               
+                          string=base_url)[:-1]}{sex}'''              
 
         return url
     
@@ -154,26 +170,25 @@ class TFRRSScraper():
             sex = self.sex
 
         if sex is None:
-          urls_for_seasons_dict = []
+          urls_for_seasons_dict = {}
           for key in seasons_dict:
-              urls_for_seasons_dict += self.produce_outcome_url_by_season_key(season_idx=key, 
+              urls_for_seasons_dict = urls_for_seasons_dict | {f'{key}_m_{self.outcome_event}': self.produce_outcome_url_by_season_key(season_idx=key, 
                                                                             sex='m', 
-                                                                            outcome_event=outcome_event)
+                                                                            outcome_event=outcome_event)}
               
-              urls_for_seasons_dict += self.produce_outcome_url_by_season_key(season_idx=key, 
+              urls_for_seasons_dict = urls_for_seasons_dict | {f'{key}_f_{self.outcome_event}': self.produce_outcome_url_by_season_key(season_idx=key, 
                                                                             sex='f', 
-                                                                            outcome_event=outcome_event)
+                                                                            outcome_event=outcome_event)}
           return urls_for_seasons_dict
         
         else:
-            urls_for_seasons_dict = []
+            urls_for_seasons_dict = {}
             for key in seasons_dict:
-                urls_for_seasons_dict += self.produce_outcome_url_by_season_key(season_idx=key, 
+                urls_for_seasons_dict = urls_for_seasons_dict | {f'{key}_{sex}_{self.outcome_event}': self.produce_outcome_url_by_season_key(season_idx=key, 
                                                                               sex=sex, 
-                                                                              outcome_event=outcome_event)
+                                                                              outcome_event=outcome_event)}
             return urls_for_seasons_dict
 
-    
     
     def produce_predictor_urls_by_season_key(self,
                                          season_idx: str,
@@ -184,7 +199,10 @@ class TFRRSScraper():
         base_url = self.seasons_dict[season_idx]
         season = season_idx.split('_')[0]
 
-        url_list = []
+        if event_list is None:
+            event_list = self.predictor_events
+
+        url_dict = {}
         for event in event_list:
             # Logic for special event cases
             match season:
@@ -206,11 +224,12 @@ class TFRRSScraper():
             if event in ['400H', '3000SC', '10000'] and season == 'indoor':
                 continue
 
-            url_list += [f'''{re.sub(pattern=r'event_type=[0-9]{2}', 
-                                     repl=self.event_dict[key], 
-                                     string=base_url)[:-1]}{sex}''']                
+            url_dict = url_dict | {f'{season_idx}_{sex}_{event}': 
+                                                f'''{re.sub(pattern=r'event_type=[0-9]{2}', 
+                                                    repl=self.event_dict[key], 
+                                                    string=base_url)[:-1]}{sex}'''}                
 
-        return url_list
+        return url_dict
     
 
     def produce_predictor_urls_by_season_dict(self, 
@@ -229,91 +248,150 @@ class TFRRSScraper():
             sex = self.sex
 
         if sex is None:
-          urls_for_seasons_dict = []
-          for key in seasons_dict:
-              urls_for_seasons_dict += self.produce_predictor_urls_by_season_key(season_idx=key, 
-                                                                            sex='m', 
-                                                                            event_list=event_list)
-              
-              urls_for_seasons_dict += self.produce_predictor_urls_by_season_key(season_idx=key, 
-                                                                            sex='f', 
-                                                                            event_list=event_list)
-              
-          return urls_for_seasons_dict
+            urls_for_seasons_dict = {}
+            for key in seasons_dict:
+                urls_for_seasons_dict = urls_for_seasons_dict | self.produce_predictor_urls_by_season_key(season_idx=key, 
+                                                                              sex='m', 
+                                                                              event_list=event_list)
+                
+                urls_for_seasons_dict = urls_for_seasons_dict | self.produce_predictor_urls_by_season_key(season_idx=key, 
+                                                                              sex='f', 
+                                                                              event_list=event_list)
+                
+            return urls_for_seasons_dict
         
         else:
-            urls_for_seasons_dict = []
+            urls_for_seasons_dict = {}
             for key in seasons_dict:
-                urls_for_seasons_dict += self.produce_predictor_urls_by_season_key(season_idx=key, 
+                urls_for_seasons_dict = urls_for_seasons_dict | self.produce_predictor_urls_by_season_key(season_idx=key, 
                                                                               sex=sex,
                                                                               event_list=event_list)
                 
             return urls_for_seasons_dict
 
-    # def download_and_clean_single_event(self, 
-    #                           url: str, 
-    #                           sex: str,
-    #                           event: str,
-    #                           season: str) -> pd.DataFrame:
-    #     '''Extract data from a given tfrrs.org URL.
+
+    def convert_event(self, df: pd.DataFrame, key: str) -> pd.DataFrame:
+        '''Take a pd.DataFrame of a TFRRS table and convert the time column into seconds
         
-    #     Parameters:
-    #       -  url (`str`): the URL to a TFRRS table
-    #       -  sex (`str`): 'f' for women and 'm' for men
-
-    #     Returns:
-    #       -  df (`pd.DataFrame`): DataFrame containing athletes who ran the 400 and 800 during the 2024 indoor season and had both times in the top 500 performances of a given division
-    #     '''
-
-    #     # Download
-    #     df = pd.read_html(f'{url[:-1]}{sex}',
-    #                                 attrs={'id': 'myTable'})[0][['Athlete', 'Team', 'Time']]
-
-
-    #     # Data Cleaning
-    #     # match event:
-    #     #     case '100':
-    #     #       df[f'time_{event}'] = 
-    #     #     case '110H':
-                                            
-    #     #     case '200':
-    #     #     case '400':
-    #     #     case '400H':
-    #     #     case '800':
-    #     #     case '800':
-    #     #     case '800':
-    #     #     case '800':
-    #     # Translate 800m time to seconds, remove '@' and '#' tags
-    #     df['Time'] = 60 * df['Time_x'].str[0].astype('float') + df['Time_x'].str[2:7].astype('float')
-
-    #     # Translate 400m time to seconds if necessary, remove '@' and '#' tags in 400m
-    #     if self.predictor_events == 400:
-    #       if df['Time_y'].dtype == 'O':
-    #           df['Time_y'] = df['Time_y'].apply(self.clean_400m_times)
-    #     # Translate 1500m/mile time to seconds if necessary, remove '@' and '#' tags in 400m
-    #     elif self.predictor_events == 1500:
-    #       df['Time_y'] = 60 * df['Time_y'].str[0].astype('float') + df['Time_y'].str[2:7].astype('float')
-
-    #     # Make columns more intuitive
-    #     df.columns = ['athlete', 'team', 'time_800', f'time_{self.predictor_events}']
-
-    #     df['athlete_team'] = df['athlete'] + ' ' + df['team']
-    #     df = df.drop(columns = ['athlete', 'team'])
-
-    #     return df[['athlete_team', 'time_800', f'time_{self.predictor_events}']]
-
-
-    # def clean_400m_times(self, row: str | float) -> float:
-    #     '''Take each row of a pd.Series and modify it according to its format
+        Parameters:
+          -  df (`pd.DataFrame`): the output of pd.read_html() directly from a TFRRS table URL
+          -  key (`str`): the key of the url dictionary that produced the URL to the TFRRS table
+          
+        Returns:
+          -  df (`pd.DataFrame`): the same dataframe, but with the time column converted to seconds from `%m:%s.%f`
+        '''
         
-    #     Parameters:
-    #       -  row (str | float): the value of the row in the pd.Series
-    #     '''
+        event = key.split('_')[3]
 
-    #     if str(row)[0] in ['4', '5']:
-    #         return float(str(row)[:5])
-    #     else:
-    #         return 60 * int(str(row)[0]) + float(str(row)[2:7])
+        match event:
+            case '100' | '110H' | '200':
+                df[f'time_{event}'] = df[f'time_{event}'].astype('float')
+
+            case '400' | '400H':
+                df[f'time_{event}'] = df[f'time_{event}'].apply(self.clean_400m_times)
+
+            case _:
+                df[f'time_{event}'] = df[f'time_{event}'].apply(self.clean_distance_times) 
+
+        return df
+
+
+    def download_single_outcome_table(self, url: str) -> pd.DataFrame:
+        '''Extract data from a given tfrrs.org URL.
+        
+        Parameters:
+          -  url (`str`): the URL to a TFRRS table
+        '''
+
+        df = pd.read_html(url, attrs={'id': 'myTable'})[0][['Athlete', 'Team', 'Time']]
+
+        df[[f'time_{self.outcome_event}', f'{self.outcome_event}_tag']] = df['Time'].str.split(' ', n=1, expand=True)
+        df['athlete_team'] = df['Athlete'] + ' | ' + df['Team']
+
+        df_format = ['athlete_team', f'time_{self.outcome_event}', f'{self.outcome_event}_tag']
+        
+        return df.drop(columns=['Athlete', 'Team', 'Time'])[df_format]
+    
+
+    def download_single_predictor_table(self, url: str, key: str) -> pd.DataFrame:
+        '''Docstring'''
+
+        df = pd.read_html(url, attrs={'id': 'myTable'})[0][['Athlete', 'Team', 'Time']]
+
+        
+
+        return df.to_dict()
+
+    
+
+    def download_outcome_event(self, export: bool = False) -> pd.DataFrame | None:
+        '''Combine all TFRRS tables of outcome variable.
+        
+        Parameters:
+          -  export (`bool`): if True, the outcome event's table is exported to the data folder and no pd.DataFrame is returned. If False, the data table is returned as a pd.DataFrame and not exported.'''
+
+        url_dict = self.produce_outcome_urls_by_season_dict()
+        dfs = None
+
+        for i in url_dict:
+            if url_dict[i] is None:
+                continue
+
+            key_vars = i.split('_')
+
+            df = self.download_single_outcome_table(url_dict[i]) \
+                .assign(sex=key_vars[2],
+                        season=f'{key_vars[0]}_{key_vars[1]}') \
+                .pipe(self.convert_event, key=i)
+                        
+            if dfs is None:
+                dfs = df
+                continue
+
+            dfs = pd.concat([dfs, df])
+
+        if export:
+            season_range = dfs['season'].unique()[[0, -1]]
+            export_path = f'data2/tfrrs_{self.division}_{self.outcome_event}_{season_range[0]}-{season_range[1]}_{datetime.now():%Y-%m-%d}.csv'
+            dfs.to_csv(export_path, index=False)
+            return
+        
+        return dfs.reset_index(drop=True)
+    
+
+    def download_and_export_data(self) -> None:
+
+        if not self.merge:
+            if self.predictor_events is None:
+                self.download_outcome_event(export=True)
+                
+            self.download_outcome_event()
+            # self.download_predictor_events()
+
+
+    def clean_400m_times(self, row: str | float) -> float:
+        '''Take each row of a pd.Series and modify it according to its format
+        
+        Parameters:
+          -  row (str | float): the value of the row in the pd.Series
+        '''
+
+        if str(row)[0] in ['4', '5']:
+            return float(str(row))
+        else:
+            time_split = str(row).split(':')
+            return 60 * float(time_split[0]) + float(time_split[1])
+        
+    
+    def clean_distance_times(self, row: str | float) -> float:
+        '''Take each row of a pd.Series and modify it according to its format
+        
+        Parameters:
+          -  row (str | float): the value of the row in the pd.Series
+        '''
+
+        time_split = str(row).split(':')
+        return 60 * float(time_split[0]) + float(time_split[1])
 
 
     # def download_and_merge_sex_data(self, url: str) -> pd.DataFrame:
